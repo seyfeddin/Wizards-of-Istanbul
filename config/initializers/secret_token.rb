2166fc5e24e29654e66824752c5c50c8e08903c2
Seyfeddin Bassarac <seyfoyun@gmail.com>
2011-03-15 19:21:50 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woi::Application.config.secret_token = '8a4687986570b3fc671dc088b38f7c705ce9faedd220097df4eb4dda868baed9527fcda08756477af3301467ec0f82bc171ef6d3e5468d7f40ea50bea9344558'
